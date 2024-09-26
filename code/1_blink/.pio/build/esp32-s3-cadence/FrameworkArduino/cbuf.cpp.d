ad24018f84cda15bb3d5fc8f2d3332b3f9d2e092
Tanmay <tanmaychhatbar@gmail.com>
2024-09-26 19:00:34 -0400

    untrack cache files

--- code/1_blink/.pio/build/esp32-s3-cadence/FrameworkArduino/cbuf.cpp.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32-s3-cadence/FrameworkArduino/cbuf.cpp.o: \
- C:/Users/Tanmay/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
- C:/Users/Tanmay/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
--- code/2_accel_print/.pio/build/esp32-s3-cadence/FrameworkArduino/firmware_msc_fat.c.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32-s3-cadence/FrameworkArduino/firmware_msc_fat.c.o: \
- C:/Users/Tanmay/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.c \
- C:/Users/Tanmay/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.h
